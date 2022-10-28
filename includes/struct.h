18e86208f611fb93331e7e2a41259d7b383f8848
Mateo Corso <mcorso@student.42.fr>
2022-10-28 22:44:49 +0200

    Je me suis trompé Gaby, on n'aura pas une table de 2 redirections seulement, d;esolé

--- includes/struct.h
+++ includes/struct.h
@@ -6,27 +6,12 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 15:54:36 by mcorso            #+#    #+#             */
-/*   Updated: 2022/10/28 21:24:46 by mcorso           ###   ########.fr       */
+/*   Updated: 2022/10/28 22:44:23 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 # define STRUCT_H
-
-
-/*		REDIRECTION STRUCT		*/
-enum	e_redirection_type
-{
-	INPUT,
-	OUPUT,
-	APPEND
-};
-
-typedef struct s_redirection
-{
-	char					*file_path;
-	enum e_redirection_type	type;
-}				t_redirection;
 
 	/*		GLOBAL STRUCT			*/
 typedef struct s_global
@@ -35,7 +20,7 @@ typedef struct s_global
 	int						standard_output;
 	char					*gobal_environment;
 	struct s_garbage_node	*garbage_collector_chain;
-	struct s_redirection	redirection_table[2];
+	struct s_redirection	*redirection_table;
 	struct s_lexer_node		*lexer_output_chain;
 	struct s_exec_node		*execution_chain;
 }				t_global;
@@ -56,6 +41,21 @@ typedef struct s_lexer_node
 	char				*word;
 	struct s_lexer_node	*next;
 }				t_lexer_node;
+
+/*		REDIRECTION CHAIN		*/
+enum	e_redirection_type
+{
+	INPUT,
+	OUPUT,
+	APPEND
+};
+
+typedef struct s_redirection
+{
+	char					*file_path;
+	enum e_redirection_type	type;
+}				t_redirection;
+
 
 /*		EXECUTION CHAIN			*/
 typedef struct s_exec_node
