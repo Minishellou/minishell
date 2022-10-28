5ba27f4c41937074c355252f46e2ac2ae07a6ecd
ArenorRaxx <mateocorso@gmail.com>
2022-10-28 16:33:05 +0200

    refactor(includes): :building_construction: Defining a set of struct for intra-communication in the shell
    Here we define a global struct, which will be used much later in the project, and basic nodes for chained lists of lexer and execution

---
+++ includes/struct.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 15:54:36 by mcorso            #+#    #+#             */
+/*   Updated: 2022/10/28 16:29:03 by mcorso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*		GLOBAL STRUCT			*/
+typedef struct	s_global
+{
+	int						standard_input;
+	int						standard_output;
+	char					*gobal_environment;
+	struct s_redirection	redirection_table[2];
+	struct s_lexer_node		*lexer_output;
+	struct s_exec_node		*execution_chain;
+};
+
+/*		REDIRECTION STRUCT		*/
+enum	redirection_type
+{
+	INPUT,
+	OUPUT,
+	APPEND
+};
+
+typedef struct	s_redirection
+{
+	char				*file_path;
+	redirection_type	type;
+}				t_redirection;
+
+/*		EXECUTION CHAIN			*/
+typedef struct	s_exec_node
+{
+	char				*command_path;
+	char				*command_args;
+	struct s_exec_node	*next;
+}				t_exec_node;
