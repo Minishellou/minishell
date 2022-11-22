0a5992b9e6b00c0dc0009f1c935c81917aa0beb6
ArenorRaxx <mateocorso@gmail.com>
2022-11-22 12:32:25 +0100

    fix(src): :bug: The envar expansion won't segfault when the provided envar doesnt exist
    If there is an envar expression in a string and it does not belong in the environment yet, the process will ignore the associated t_lexer_node, which would have its word member set to NULL

--- src/envar_expansion/envar_expansion.c
+++ src/envar_expansion/envar_expansion.c
@@ -6,7 +6,7 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:46:54 by mcorso            #+#    #+#             */
-/*   Updated: 2022/11/21 13:48:29 by mcorso           ###   ########.fr       */
+/*   Updated: 2022/11/22 12:22:05 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,39 @@
 
 t_env_node	*get_envar(char *envar_name)
 {
-	t_env_node	*env_node;
+	size_t		envar_name_len;
+	t_env_node	env;
+	t_env_node	*current_node;
 
-	env_node = g_glo.env;
-	while (env_node && \
-			ft_strncmp(env_node->name, envar_name + 1, ft_strlen(env_node->name)) != 0)
-		env_node = env_node->next;
-	return (env_node);
+	env.next = g_glo.env;
+	current_node = &env;
+	envar_name_len = ft_strlen(envar_name) - 1;
+	while (1)
+	{
+		current_node = current_node->next;
+		if (!current_node)
+			break ;
+		if (ft_strncmp(current_node->name, envar_name + 1, envar_name_len))
+			continue;
+		if (envar_name_len != ft_strlen(current_node->name))
+			continue;
+		break ;
+	}
+	return (current_node);
 }
 
 static t_lexer_node	**find_envar_node(t_lexer_node **first_node)
 {
-	t_lexer_node	**current_node;
+	t_lexer_node	*current_node;
+	t_lexer_node	**ret_node;
 
-	current_node = &(*first_node);
-	while (*(*current_node)->word != '$')
-		*current_node = (*current_node)->next;
-	return (current_node);
+	current_node = *first_node;
+	if (*(*first_node)->word == '$')
+		return (first_node);
+	while (*current_node->next->word != '$')
+		current_node = current_node->next;
+	ret_node = &current_node->next;
+	return (ret_node);
 }
 
 int	expand_and_add_to_string(char **current_string, t_env_node *envar)
@@ -44,7 +60,6 @@ int	expand_and_add_to_string(char **current_string, t_env_node *envar)
 	envar_node = find_envar_node(&first_node);
 	if (expand_and_add_to_chain(envar_node, envar) != SUCCESS)
 		return (ERROR);
-	write(1, "ok\n", 3);
 	*current_string = concat_chain_to_string(first_node);
 	if (!*current_string)
 		return (ERROR);
@@ -56,15 +71,15 @@ int	expand_and_add_to_chain(t_lexer_node **envar_node, t_env_node *envar)
 	char			**splited_envar;
 	t_lexer_node	*subchain;
 
-	splited_envar = ft_split(envar->value, ' ');
-	write(1, "ok\n", 3);
-	if (!splited_envar)
-		return (ERROR);
-	subchain = (t_lexer_node *)make_chain_from_array(splited_envar, create_lexer_node);
-	write(1, "ok\n", 3);
+	subchain = NULL;
+	if (envar)
+		splited_envar = ft_split(envar->value, ' ');
+	if (envar && splited_envar)
+		subchain = (t_lexer_node *)make_chain_from_array(splited_envar, create_lexer_node);
+	if (!envar)
+		subchain = (t_lexer_node *)create_lexer_node(NULL);
 	if (!subchain)
 		return (ERROR);
 	include_subchain_at((t_node **)envar_node, (t_node *)subchain);
-	write(1, "ok\n", 3);
 	return (SUCCESS);	
 }
