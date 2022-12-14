ec87be4f00c8a288fdd0971de47f024767935f28
Mateo Corso <mcorso@student.42.fr>
2022-12-14 14:24:06 +0100

    style(src): Norm

--- src/executor/io_env_manager.c
+++ src/executor/io_env_manager.c
@@ -6,7 +6,7 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 11:57:25 by mcorso            #+#    #+#             */
-/*   Updated: 2022/12/04 11:57:51 by mcorso           ###   ########.fr       */
+/*   Updated: 2022/12/14 14:24:06 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,26 @@ static void	open_append_redir(char *file_path, t_io_env *to_fill);
 
 int	io_environment_manager(t_exec_node *current_command)
 {
-	t_io_env		current_io_env;
+	t_io_env		ret_io_env;
 	t_redirection	*current_redirection_node;
 
-	current_io_env.input = dup(0);
-	current_io_env.output = dup(1);
+	ret_io_env.input = dup(0);
+	ret_io_env.output = dup(1);
 	current_redirection_node = current_command->redir_chain;
 	while (current_redirection_node != NULL)
 	{
 		if (current_redirection_node->type == INPUT)
-			open_input_redir(current_redirection_node->argument, &current_io_env);
+			open_input_redir(current_redirection_node->argument, &ret_io_env);
 		if (current_redirection_node->type == OUTPUT)
-			open_output_redir(current_redirection_node->argument, &current_io_env);
+			open_output_redir(current_redirection_node->argument, &ret_io_env);
 		if (current_redirection_node->type == APPEND)
-			open_append_redir(current_redirection_node->argument, &current_io_env);
-		if (current_io_env.input == ERROR || current_io_env.output == ERROR)
+			open_append_redir(current_redirection_node->argument, &ret_io_env);
+		if (ret_io_env.input == ERROR || ret_io_env.output == ERROR)
 			return (ERROR);
 		current_redirection_node = current_redirection_node->next;
 	}
-	current_command->io_env.input = current_io_env.input;
-	current_command->io_env.output = current_io_env.output;
+	current_command->io_env.input = ret_io_env.input;
+	current_command->io_env.output = ret_io_env.output;
 	return (SUCCESS);
 }
 
